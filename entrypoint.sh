96539212d32bb50e541939b6518907b18c8a50d4
Alexandre Antunes <AlexAntn@users.noreply.github.com>
2022-03-23 12:06:36 +0100

    Added hub configuration step

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,6 +22,9 @@ DESTINATION_BRANCH="${INPUT_DESTINATION_BRANCH:-"master"}"
 # Github actions no longer auto set the username and GITHUB_TOKEN
 git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@${GITHUB_SERVER_URL#https://}/$GITHUB_REPOSITORY"
 
+# configure hub to have the same credentials
+sed 's/.*oauth_token: .*/  oauth_token: $GITHUB_TOKEN/' $HOME/.config/hub
+
 # Pull all branches references down locally so subsequent commands can see them
 git fetch origin '+refs/heads/*:refs/heads/*' --update-head-ok
 
